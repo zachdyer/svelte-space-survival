f221347da8a2a63e2bb66d366f85122cafad1ddd
Zach Dyer <zach.dyer@gmail.com>
2023-01-20 15:11:14 -0600

    Updates

--- src/lib/Core.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import Component from './Component.svelte'
-</script>
-
-<Component 
-  title="Core" 
-  text="Vehicle cores are required for a vehicle to function." 
-  img="/img/core.webp"
-/>
--- src/lib/stores.js
+++
@@ -1,3 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const batteryLevel = writable(0);
